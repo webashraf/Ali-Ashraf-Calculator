8c97d08f2ae235b302075f250ed9ee3e9bbb58c7
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2021-12-07 21:13:24 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+
+document.getElementById('clearButton').addEventListener('click', function(){
+    document.getElementById('displayNumber').innerText = ' ';
+})
+
+document.getElementById('zeroToTen9').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen9').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+
+document.getElementById('zeroToTen8').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen8').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementById('zeroToTen7').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen7').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementsByClassName('numButtons').addEventListener('click', function(){
+    const value = document.getElementsByClassName('numButtons').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+
+
+document.getElementById('zeroToTen6').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen6').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementById('zeroToTen5').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen5').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementById('zeroToTen4').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen4').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementById('zeroToTen3').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen3').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementById('zeroToTen2').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen2').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementById('zeroToTen1').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen1').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementById('zeroToTen0').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen0').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+document.getElementById('zeroToTen00').addEventListener('click', function(){
+    const value = document.getElementById('zeroToTen00').innerText;
+    document.getElementById('displayNumber').innerText = value;
+})
+
